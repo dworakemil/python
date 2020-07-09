28a83530ac1d72c04f257c25b45cf94ac5df738b
Dworak Emil <61839168+dworakemil@users.noreply.github.com>
2020-07-09 12:06:30 +0200

    Add files via upload
    Fraktale Sierpińskiego

---
+++ sierpinski.py
@@ -0,0 +1,55 @@
+import pygame, sys, math, random
+
+
+width = height = 1000
+
+triangle_side = width
+triangle_height = (triangle_side * math.sqrt(3))/ 2
+iterations = 5
+
+pygame.init()
+
+canvas = pygame.display.set_mode( ( width, height ) )
+
+
+finished = False
+clock = pygame.time.Clock()
+
+def drawLine( a, b ):
+    pygame.draw.line( canvas, (255,255,255), a, b )
+
+def drawTriangle( a, b, c ):
+    drawLine( a, b )
+    drawLine( b, c )
+    drawLine( c, a )
+
+def getCenter( a, b ):
+    center = ( (a[0]+b[0])/2, (a[1]+b[1])/2 )
+    return center
+
+def frac( order, a, b, c ):
+    if( order == 0 ):
+        drawTriangle( a, b, c )
+    else:
+        
+        ab = getCenter( a, b )
+        bc = getCenter( b, c )
+        ca = getCenter( c, a )
+
+        frac( order-1, a, ab, ca )
+        frac( order-1, ab, b, bc )
+        frac( order-1, bc, c, ca )
+        
+        pass
+
+frac( 8, (0,triangle_height), (triangle_side/2,0), (triangle_side, triangle_height) )
+
+
+pygame.display.flip()
+
+    
+
+
+
+pygame.quit()
+quit()
